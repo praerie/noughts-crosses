97a04ec51eb3ac605da1cb48dff55d48abfd72cd
praerie <elisabethadelie@gmail.com>
2024-02-25 19:34:15 -0500

    add winning conditions

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ class Program
         grid = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         bool firstPlayerTurn = true;
 
-        while (true) 
+        while (!HasWinner()) 
         {
             PrintGrid(grid);
 
@@ -59,6 +59,24 @@ class Program
 
             // toggle turn btwn first and second players
             firstPlayerTurn = !firstPlayerTurn;
+        }
+
+        bool HasWinner()
+        {
+            // checking for all Xs or all Os
+            // rows
+            bool row1 = grid[0] == grid[1] && grid[1] == grid[2];
+            bool row2 = grid[3] == grid[4] && grid[4] == grid[5]; 
+            bool row3 = grid[6] == grid[7] && grid[7] == grid[8]; 
+            // columns
+            bool col1 = grid[0] == grid[3] && grid[3] == grid[6]; 
+            bool col2 = grid[1] == grid[4] && grid[4] == grid[7];
+            bool col3 = grid[2] == grid[5] && grid[5] == grid[8];
+            // diagonally 
+            bool diagDown = grid[0] == grid[4] && grid[4] == grid[8]; 
+            bool diagUp = grid[6] == grid[4] && grid[4] == grid[2];
+
+            return row1 || row2 || row3 || col1 || col2 || col3 || diagDown || diagUp;
         }
     }
 }
