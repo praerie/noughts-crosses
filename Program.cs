0478eae3c2c945a68d44d21e4b299549ce38d3b3
praerie <elisabethadelie@gmail.com>
2024-02-25 20:39:55 -0500

    add play again loop

--- Program.cs
+++ Program.cs
@@ -7,64 +7,95 @@ class Program
 
     static void Main(string[] args)
     {
-        grid = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-        bool firstPlayerTurn = true;
-        int turnCount = 0;
+        bool playAgain = true;
 
-        while (!HasWinner() && turnCount != 9) 
+        while (playAgain) 
         {
-            PrintGrid(grid);
+            grid = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            bool firstPlayerTurn = true;
+            int turnCount = 0;
 
-            if (firstPlayerTurn) 
-                Console.Write("First player's turn\n> ");
-            else
-                Console.Write("Second player's turn\n> ");
-
-            string move = Console.ReadLine();
-
-            if (grid.Contains(move) && move != "X" && move != "O")
+            while (!HasWinner() && turnCount != 9) 
             {
-                int gridIndex = Convert.ToInt32(move) - 1;
-                
-                if (firstPlayerTurn)
-                {
-                    grid[gridIndex] = "X";
-                }   
-                else
-                    grid[gridIndex] = "O";
+                PrintGrid(grid);
 
-                turnCount++;
+                if (firstPlayerTurn) 
+                    Console.Write("\nCROSS player's turn\n> ");
+                else
+                    Console.Write("\nNOUGHT player's turn\n> ");
+
+                string move = Console.ReadLine();
+
+                if (grid.Contains(move) && move != "X" && move != "O")
+                {
+                    int gridIndex = Convert.ToInt32(move) - 1;
+                    
+                    if (firstPlayerTurn)
+                    {
+                        grid[gridIndex] = "X";
+                    }   
+                    else
+                        grid[gridIndex] = "O";
+
+                    turnCount++;
+                }
+
+                // toggle turn btwn first and second players
+                firstPlayerTurn = !firstPlayerTurn;
             }
 
-            // toggle turn btwn first and second players
-            firstPlayerTurn = !firstPlayerTurn;
+            if(HasWinner()) 
+                Console.WriteLine("\n~* We have a winner! *~");
+            else
+                Console.WriteLine("\n> It's a tie! <"); 
+            
+            PrintGrid(grid);
+
+            bool validInput = false;
+            while (!validInput)
+            {
+                Console.Write("\nDo you want to play again? (Y/N)> ");
+                string playAgainInput = Console.ReadLine().ToLower();
+
+                try
+                {
+                    if (playAgainInput == "y")
+                        validInput = true;
+                    else if (playAgainInput == "n") 
+                    {
+                        playAgain = false;
+                        validInput = true;
+                        Console.WriteLine("\nThank you for playing!\n");
+                    }
+                    else
+                        throw new Exception("Invalid input! Please enter Y or N.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
+    }
 
-        if(HasWinner()) 
-            Console.WriteLine("We have a winner!");
-        else
-            Console.WriteLine("It's a tie!"); 
-        
-        PrintGrid(grid);
+    static bool HasWinner()
+    {
+        // checking for all Xs or all Os across rows, columns, and diagonals 
+        bool row1 = grid[0] == grid[1] && grid[1] == grid[2];
+        bool row2 = grid[3] == grid[4] && grid[4] == grid[5]; 
+        bool row3 = grid[6] == grid[7] && grid[7] == grid[8]; 
+        bool col1 = grid[0] == grid[3] && grid[3] == grid[6]; 
+        bool col2 = grid[1] == grid[4] && grid[4] == grid[7];
+        bool col3 = grid[2] == grid[5] && grid[5] == grid[8];
+        bool diagDown = grid[0] == grid[4] && grid[4] == grid[8]; 
+        bool diagUp = grid[6] == grid[4] && grid[4] == grid[2];
 
-        bool HasWinner()
-        {
-            // checking for all Xs or all Os across rows, columns, and diagonals 
-            bool row1 = grid[0] == grid[1] && grid[1] == grid[2];
-            bool row2 = grid[3] == grid[4] && grid[4] == grid[5]; 
-            bool row3 = grid[6] == grid[7] && grid[7] == grid[8]; 
-            bool col1 = grid[0] == grid[3] && grid[3] == grid[6]; 
-            bool col2 = grid[1] == grid[4] && grid[4] == grid[7];
-            bool col3 = grid[2] == grid[5] && grid[5] == grid[8];
-            bool diagDown = grid[0] == grid[4] && grid[4] == grid[8]; 
-            bool diagUp = grid[6] == grid[4] && grid[4] == grid[2];
-
-            return row1 || row2 || row3 || col1 || col2 || col3 || diagDown || diagUp;
-        }
+        return row1 || row2 || row3 || col1 || col2 || col3 || diagDown || diagUp;
     }
 
     static void PrintGrid(string[] grid) 
     {
+        Console.WriteLine("");
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -97,7 +128,7 @@ class Program
             // add horizontal separator if not last row
             if (i < 2)
             {
-                Console.WriteLine("-----------");
+                Console.WriteLine(new string('-', 11));
             }
         }
     }
